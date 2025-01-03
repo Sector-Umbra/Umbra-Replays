d10ab506195781aabf7d424c5fc1db104d0701ec
Simon <63975668+Simyon264@users.noreply.github.com>
2025-01-04 00:01:49 +0100

    Implement replay downloading
    This implementation uses the file names and paths. While these are exposed to the user, I believe this is easier as running regex on every replay to get the round ID or something else seems more annoying than its worth.

---
+++ SU.Replays/Controllers/DownloadController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SU.Replays.Database;
+using SU.Replays.Helpers;
+
+namespace SU.Replays.Controllers;
+
+public class DownloadController : Controller
+{
+    private ApplicationDbContext _dbContext;
+    private ReplayHelper _replayHelper;
+
+    public DownloadController(ApplicationDbContext dbContext, ReplayHelper replayHelper)
+    {
+        _dbContext = dbContext;
+        _replayHelper = replayHelper;
+    }
+
+    [Authorize]
+    [HttpGet("download-replay")]
+    public async Task<IActionResult> Download(
+        [FromQuery] string file
+    )
+    {
+        var replay = await _dbContext.Replays
+            .AsNoTracking()
+            .FirstOrDefaultAsync(r => r.FileLocation == file);
+
+        if (replay == null)
+            return NotFound();
+
+        var userId = _replayHelper.GetSsId(User);
+        var groups = _replayHelper.GetGroups(User);
+        if (!replay.Participants.Contains(userId) &&
+            ReplayHelper.AllAccessRoles.All(r => !groups.Contains(r))
+            )
+        {
+            return Forbid();
+        }
+
+        var rootPath = Path.GetFullPath(replay.FileLocation);
+        return PhysicalFile(rootPath, "application/zip", Path.GetFileName(rootPath));
+    }
+}
